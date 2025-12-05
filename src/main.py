d535fec76f90cd175d84325e6199550c1d43f907
juanklv2003 <carloslorenzovillar0@gmail.com>
2025-12-05 09:14:04 +0100

    add def upper

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 def elevar(base, exp):
     return base**exp
+
+def upper(text):
+    return text.upper()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,7 @@
-from src.main import elevar 
+from src.main import elevar, upper
 
 def test_elevar_basico():
     assert elevar(2,3)==8
 
-
+def test_to_upper():
+    assert upper("Carlos")=="CARLOS"
