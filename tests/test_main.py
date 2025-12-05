4bd95adcbfbccad8089443ab4770bda63de66e95
juanklv2003 <carloslorenzovillar0@gmail.com>
2025-12-05 09:46:40 +0100

    add hola2

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,10 @@
-from src.main import elevar, upper
+from src.main import elevar, upper, hola
 
 def test_elevar_basico():
     assert elevar(2,3)==8
 
 def test_to_upper():
     assert upper("Carlos")=="CARLOS"
+
+def test_hola():
+    assert  hola=="adios"
